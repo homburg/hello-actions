309e8dd6f37ebec8028a31ff3604cd1b7f35aaca
Thomas B Homburg <thomas@madethis.dk>
2022-11-04 14:06:15 +0100

    outputs.output

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ export function add(a: number, b: number): number {
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3))
+  // console.log("Add 2 + 3 =", add(2, 3))
 
-  console.log(Deno.args)
+  console.log(`version=${Deno.args}`)
 }
